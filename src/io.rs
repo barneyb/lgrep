b2616ddd543b0079e342a3dfa25cc92488e1970b
Barney Boisvert <bboisvert@gmail.com>
2024-07-13 11:09:34 -0700

    bypass compress_io on windows

--- src/io.rs
+++ src/io.rs
@@ -1,23 +1,43 @@
 use std::io::BufRead;
 
 use anyhow::{Context, Result};
-use compress_io::compress::CompressIo;
 
 pub(crate) const STDIN_FILENAME: &str = "-";
 
+/// Open a [BufRead] for the named file, or STDIN if the filename is '-'. If the
+/// stream is compressed using a well-known format (e.g. gzip), it will be
+/// decompressed automatically _on Unix-ish platforms_, by shelling out to an
+/// appropriate utility on your `$PATH`. On Windows, you must manually
+/// decompress the stream/file first.
 pub fn get_reader(filename: &String) -> Result<Box<dyn BufRead>> {
-    Ok(if filename == STDIN_FILENAME {
-        Box::new(
-            CompressIo::new() // implicitly STDIN
-                .bufreader()
-                .with_context(|| "Failed to open STDIN for reading")?,
-        )
+    if filename == STDIN_FILENAME {
+        open_stdin().with_context(|| "Failed to open STDIN for reading")
     } else {
-        Box::new(
-            CompressIo::new()
-                .path(filename)
-                .bufreader()
-                .with_context(|| format!("Failed to open '{filename}' for reading"))?,
-        )
-    })
+        open_file(filename).with_context(|| format!("Failed to open '{filename}' for reading"))
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+fn open_stdin() -> Result<Box<dyn BufRead>> {
+    use compress_io::compress::CompressIo;
+    Ok(Box::new(CompressIo::new().bufreader()?))
+}
+
+#[cfg(not(target_os = "windows"))]
+fn open_file(filename: &String) -> Result<Box<dyn BufRead>> {
+    use compress_io::compress::CompressIo;
+    Ok(Box::new(CompressIo::new().path(filename).bufreader()?))
+}
+
+#[cfg(target_os = "windows")]
+fn open_stdin() -> Result<Box<dyn BufRead>> {
+    use std::io::stdin;
+    Ok(Box::new(stdin().lock()))
+}
+
+#[cfg(target_os = "windows")]
+fn open_file(filename: &String) -> Result<Box<dyn BufRead>> {
+    use std::fs::File;
+    use std::io::BufReader;
+    Ok(Box::new(BufReader::new(File::open(filename)?)))
 }
